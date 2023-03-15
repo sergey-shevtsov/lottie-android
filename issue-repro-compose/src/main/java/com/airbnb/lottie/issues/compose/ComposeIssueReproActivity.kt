525007c0621671f67423b9e9ac20bab3655e71ef
Sergey Shevtsov <ssh@kode.ru>
2023-03-15 14:08:30 +0200

    reproduce compose animation height issue

--- issue-repro-compose/src/main/java/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
+++ issue-repro-compose/src/main/java/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
@@ -3,8 +3,16 @@ package com.airbnb.lottie.issues.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -21,8 +29,25 @@ class ComposeIssueReproActivity : AppCompatActivity() {
 
     @Composable
     fun Content() {
-        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.heart))
-        val progress by animateLottieCompositionAsState(composition, iterations = LottieConstants.IterateForever)
-        LottieAnimation(composition, { progress })
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.nature))
+            val progress by animateLottieCompositionAsState(composition, iterations = LottieConstants.IterateForever)
+            LottieAnimation(
+                modifier = Modifier.fillMaxWidth(),
+                composition = composition,
+                progress = { progress },
+                contentScale = ContentScale.FillWidth,
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Other content",
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
